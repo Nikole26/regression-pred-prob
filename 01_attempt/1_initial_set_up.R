a71ec5cd82f44841494fdfc9af5aabfb674c55d9
Nikole26 <nikolemonterocervantes2026@u.northwestern.edu>
2024-05-04 17:22:08 -0500

    initial set up done

--- 01_attempt/1_initial_set_up.R
+++ 01_attempt/1_initial_set_up.R
@@ -15,6 +15,6 @@ load(here("data/training_data.rda"))
 # create resamples (5-fold cv with 3 repeats) ----------
 set.seed(2468)
 air_bnb_folds <- training_data |>
-  vfold_cv(v = 5, repeats = 3)
+  vfold_cv(v = 5, repeats = 3, price)
 
 save(air_bnb_folds, file = here("01_attempt/data_splits/air_bnb_folds.rda"))
