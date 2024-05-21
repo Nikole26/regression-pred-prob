32cefb259ce0c7371a67a83ee1a4107c2a44b974
Nikole26 <nikolemonterocervantes2026@u.northwestern.edu>
2024-05-21 18:30:35 -0500

    attempt 9 set up files

---
+++ 09_attempt/2_recipes.R
@@ -0,0 +1,65 @@
+# Regression Problem ----
+# Setup preprocessing/recipes/feature engineering
+
+# Load package(s) ----
+library(tidyverse)
+library(tidymodels)
+library(here)
+library(recipes)
+
+# handle common conflicts
+tidymodels_prefer()
+
+# loading training data -------
+load(here("08_attempt/data/training_data.rda"))
+
+# Recipe 1
+recipe_1 <- recipe(log_price ~ ., data = training_data) |>
+  step_rm(id, host_response_time, host_verifications) |>
+  step_impute_mean(all_numeric_predictors()) |>
+  step_impute_mode(all_nominal_predictors()) |>
+  step_dummy(all_nominal_predictors(), one_hot = TRUE) |>
+  step_nzv(all_predictors()) |>
+  step_normalize(all_numeric_predictors())
+
+# Recipe 2
+recipe_2 <- recipe(log_price ~ ., data = training_data) |>
+  step_rm(id, host_response_time, host_verifications) |>
+  step_impute_mean(all_numeric_predictors()) |>
+  step_impute_mode(all_nominal_predictors()) |>
+  step_impute_knn(all_predictors()) |>
+  step_other(all_nominal_predictors(), threshold = 0.05) |>
+  step_dummy(all_nominal_predictors(), one_hot = TRUE) |>
+  step_nzv(all_predictors()) |>
+  step_normalize(all_numeric_predictors())
+
+recipe_3 <- recipe(log_price ~ ., data = training_data) |>
+  step_rm(id, host_response_time, host_verifications, first_review_year, last_review_year) |>
+  step_YeoJohnson(all_numeric_predictors()) |>
+  step_impute_mean(all_numeric_predictors()) |>
+  step_impute_mode(all_nominal_predictors()) |>
+  step_impute_knn(all_predictors()) |>
+  step_other(all_nominal_predictors(), threshold = 0.05) |>
+  step_dummy(all_nominal_predictors(), one_hot = TRUE) |>
+  step_nzv(all_predictors()) |>
+  step_normalize(all_numeric_predictors())
+
+recipe_4 <- recipe(log_price ~ ., data = training_data) |>
+  step_rm(id, host_response_time, host_verifications, first_review_year, last_review_year) |>
+  step_impute_mean(all_numeric_predictors()) |>
+  step_impute_mode(all_nominal_predictors()) |>
+  step_impute_knn(all_predictors()) |>
+  step_dummy(all_nominal_predictors(), one_hot = TRUE) |>
+  step_nzv(all_predictors()) |>
+  step_normalize(all_numeric_predictors())
+
+recipe_4 |>
+  prep() |>
+  bake(new_data = NULL) |>
+  glimpse()
+
+save(recipe_1, file = here("08_attempt/recipes/recipe_1.rda"))
+save(recipe_2, file = here("08_attempt/recipes/recipe_2.rda"))
+save(recipe_3, file = here("08_attempt/recipes/recipe_3.rda"))
+save(recipe_4, file = here("08_attempt/recipes/recipe_4.rda"))
+
---
+++ 09_attempt/4_model_comparison_1.R
@@ -0,0 +1,38 @@
+# Model analysis and comparison
+
+# Load package(s) ----
+library(tidyverse)
+library(tidymodels)
+library(tictoc)
+library(here)
+# Handle conflicts
+tidymodels_prefer()
+
+# load tune models ----
+load(here("08_attempt/results/bt_tune_1.rda"))
+load(here("08_attempt/results/bt_tune_2.rda"))
+load(here("08_attempt/results/bt_tune_3.rda"))
+load(here("08_attempt/results/bt_tune_4.rda"))
+
+# Creating table
+# all models results
+model_results <- as_workflow_set(
+  bt_1 = bt_tune_1,
+  bt_2 = bt_tune_2,
+  bt_3 = bt_tune_3,
+  bt_4 = bt_tune_4
+)
+
+models_table_1 <- model_results |>
+  collect_metrics() |>
+  filter(.metric == "mae") |>
+  slice_min(mean, by = wflow_id) |>
+  select(wflow_id, mean, std_err) |>
+  arrange(mean)
+
+model_results |>
+  autoplot(metric = "mae")
+
+model_results |>
+  autoplot(metric = "mae", select_best = TRUE)
+
