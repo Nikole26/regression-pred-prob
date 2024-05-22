8c23c4a6bf49956642790dc6f1def668a2ae7d2b
Nikole26 <nikolemonterocervantes2026@u.northwestern.edu>
2024-05-22 10:54:29 -0500

    set up attempt 10

---
+++ 10_attempt/5_fit_best_model.R
@@ -0,0 +1,27 @@
+# Regression Prediction Problem - Attempt 1 ----
+# Training best model - Rf model
+
+# load packages ----
+library(tidyverse)
+library(tidymodels)
+library(here)
+library(bonsai)
+
+# loading necessary data
+load(here("08_attempt/results/bt_tune_4.rda"))
+load(here("08_attempt/data/training_data.rda"))
+
+# Best Model --------
+select_best(bt_tune_4, metric = "mae")
+
+# finalize workflow for roc-----
+final_wflow <- bt_tune_4 |>
+  extract_workflow(bt_tune_4) |>
+  finalize_workflow(select_best(bt_tune_4, metric = "mae"))
+
+# train final model----
+set.seed(110)
+final_fit <- fit(final_wflow, training_data)
+
+# saving results-------
+save(final_fit, file = here("08_attempt/results/final_fit.rda"))
